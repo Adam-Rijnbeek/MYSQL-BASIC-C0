cbfbe821b98742fa850af03bc0041af50b79d388
Adam <adam.rijnbeek@student.rocva.nl>
2022-03-08 09:20:04 +0100

    eindopdracht sql nog een keer verbterd,

--- eindopdracht/antwoorden.sql
+++ eindopdracht/antwoorden.sql
@@ -15,9 +15,9 @@ SELECT name, genre, publisher FROM videogamesales WHERE JP_Sales=532;
 -- Opdracht 8
 SELECT SUM(Global_Sales) AS aantal_verkochte_games_nintendo FROM `videogamesales` WHERE publisher = 'Nintendo'
 -- Opdracht 9
-SELECT name , year FROM `videogamesales` WHERE genre = 'racing' AND publisher = 'Nintendo' OR publisher = 'Activision'
+SELECT name , year FROM `videogamesales` WHERE genre = 'racing' AND (publisher = 'Nintendo' OR publisher = 'Activision');
 -- Opdracht 10
-SELECT AVG(NA_Sales) , AVG(EU_Sales) , AVG(JP_Sales) AS Gemiddelde_sales_Noord_amerika_Europa_Japan FROM `videogamesales`
+SELECT AVG(NA_Sales), AVG(EU_Sales), AVG(JP_Sales) FROM videogamesales;SELECT AVG(NA_Sales) AS gemiddelde_verkoop_NA, AVG(EU_Sales) AS gemiddelde_verkoop_EU, AVG(JP_Sales) gemiddelde_verkoop_JP FROM `videogamesales`
 -- Opdracht 11
 DELETE FROM `videogamesales` WHERE name = 'Halo 2'
 -- Opdracht 12
